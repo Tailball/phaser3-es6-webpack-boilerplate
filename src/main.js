74644bd87859a168e395e619c0675810cea5f778
Jochen <jochen.panjaer@ziango.com>
2018-12-06 13:38:48 +0100

    added compiler for SASS

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 import 'phaser';
+import './sass/main.scss';
+
 import BootScene from './scenes/BootScene';
 import GameScene from './scenes/GameScene';
 import TitleScene from './scenes/TitleScene';
 
 const config = {
     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
-    type: Phaser.WEBGL,
+    type: Phaser.AUTO,
+
     pixelArt: true,
     roundPixels: true,
+    
     parent: 'content',
+    
     width: 400,
     height: 240,
+    
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {
-                y: 800
-            },
+            gravity: { y: 800 },
             debug: false
         }
     },
--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,8 @@ module.exports = {
         rules: [
             { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
             { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-            { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' }
+            { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
         ]
     },
    /* node: {
